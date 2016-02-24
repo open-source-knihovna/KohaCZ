a80df32b98862115c0c79ee0611e80439c5047ca
Josef Moravec <josef.moravec@gmail.com>
2016-02-24 08:27:59 +0100

    Added zebedee config for Z39.50 Update authorities of National Library Prague

--- debian/koha-common.cron.d
+++ debian/koha-common.cron.d
@@ -9,3 +9,6 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 # Comment the following line if you want to use the experimental koha-index-daemon integration
 */5 * * * * root test -x /usr/sbin/koha-rebuild-zebra && koha-rebuild-zebra -q $(koha-list --enabled)
 */15 * * * * root koha-foreach --enabled --email /usr/share/koha/bin/cronjobs/process_message_queue.pl
+
+# Uncomment the following line if you want use Z39.50 Update authorities of National Library Prague
+#@reboot zebedee -T 57779 8888:vega.nkp.cz:7777
