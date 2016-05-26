7fd6b21073db7a33304d6a9ec7cfccfc9e9465cb
Josef Moravec <josef.moravec@gmail.com>
2016-05-26 13:26:03 +0200

    Merge original and custom translations before deploy

--- publishKoha.sh
+++ publishKoha.sh
@@ -8,8 +8,11 @@ fi
 VERSION=$1
 SERVER=192.168.1.113
 
+msgcat misc/translator/po/cs-CZ-opac-bootstrap-orig.po misc/translator/po/cs-CZ-opac-bootstrap-kohacz.po > misc/translator/po/cs-CZ-opac-bootstrap.po
+msgcat misc/translator/po/cs-CZ-staff-prog-orig.po misc/translator/po/cs-CZ-staff-prog-kohacz.po > misc/translator/po/cs-CZ-staff-prog.po
+msgcat misc/translator/po/cs-CZ-pref-orig.po misc/translator/po/cs-CZ-pref-kohacz.po > misc/translator/po/cs-CZ-pref.po
 
-#DEB_BUILD_OPTIONS=nocheck ./debian/build-git-snapshot -r ~/debian -v $VERSION -d --noautoversion
+DEB_BUILD_OPTIONS=nocheck ./debian/build-git-snapshot -r ~/debian -v $VERSION -d --noautoversion
 
 scp "../debian/koha-deps_${VERSION}_all.deb" $SERVER:/root/kohadeb/
 scp "../debian/koha-perldeps_${VERSION}_all.deb" $SERVER:/root/kohadeb/
@@ -25,4 +28,8 @@ ssh root@192.168.1.113 <<ENDSSH
     aptly publish snapshot -architectures="i386,amd64" $VERSION
     cp -r .aptly/public/* /var/www/debian/
 ENDSSH
+
+rm misc/translator/po/cs-CZ-opac-bootstrap.po
+rm misc/translator/po/cs-CZ-staff-prog.po
+rm misc/translator/po/cs-CZ-pref.po
 
