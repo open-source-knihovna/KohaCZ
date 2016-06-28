0c67f15069c6aa460b9d6015bea435dd24c87389
Magnus Enger <magnus@libriotech.no>
2016-06-28 10:45:08 +0200

    Bug 16823 - Comment out koha-rebuild-zebra in koha-common.cron.d
    Bug 16190 enabled the indexer daemon by default for package installations.
    This means that it is no longer necessary to set up koha-rebuild-zebra as
    a cron job. This patch comments it out, so that people who might run into
    bug 16814 can easily activate the cronjob again after de-activating the
    indexer daemon.
    
    To test:
    Just read the diff and check that it makes sense.
    
    Signed-off-by: Srdjan <srdjan@catalyst.net.nz>
    
    Signed-off-by: Jonathan Druart <jonathan.druart@bugs.koha-community.org>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- debian/koha-common.cron.d
+++ debian/koha-common.cron.d
@@ -6,8 +6,9 @@
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-# Comment the following line if you want to use the experimental koha-index-daemon integration
-*/5 * * * * root test -x /usr/sbin/koha-rebuild-zebra && koha-rebuild-zebra -q $(koha-list --enabled)
+# Uncomment the following line if you do not want to use the koha-index-daemon integration
+# */5 * * * * root test -x /usr/sbin/koha-rebuild-zebra && koha-rebuild-zebra -q $(koha-list --enabled)
+
 */15 * * * * root koha-foreach --enabled --email /usr/share/koha/bin/cronjobs/process_message_queue.pl
 
 # Uncomment the following line if you want use Z39.50 Update authorities of National Library Prague
