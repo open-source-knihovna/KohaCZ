ec859c3fa31a2761eeab6af1d1d4b93e464a01af
Jesse Weaver <jweaver@bywatersolutions.com>
2016-07-07 11:02:53 -0600

    Bug 14048: (RM followup) DB version 16.06.00.006
    Signed-off-by: Jesse Weaver <jweaver@bywatersolutions.com>

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -12786,7 +12786,33 @@ if ( CheckVersion($DBversion) ) {
 INSERT INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type` ) VALUES ('PermanentWithdrawnNumberItemsColumns', '', NULL, 'Which columns of items table use to determinate the new withdraw number', 'Free');
     });
 
-    print "Upgrade to $DBversion done (Make wihtdrawal reason authorised value)\n";
+    $dbh->do(q{
+        CREATE TABLE `refund_lost_item_fee_rules` (
+          `branchcode` varchar(10) NOT NULL default '',
+          `refund` tinyint(1) NOT NULL default 0,
+          PRIMARY KEY  (`branchcode`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+    });
+    $dbh->do(q{
+        INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type)
+        VALUES( 'RefundLostOnReturnControl',
+                'CheckinLibrary',
+                'If a lost item is returned, choose which branch to pick rules for refunding.',
+                'CheckinLibrary|PatronLibrary|ItemHomeBranch|ItemHoldingbranch',
+                'Choice')
+    });
+    # Pick the old syspref as the default rule
+    $dbh->do(q{
+        INSERT INTO refund_lost_item_fee_rules (branchcode,refund)
+            SELECT '*', COALESCE(value,'1') FROM systempreferences WHERE variable='RefundLostItemFeeOnReturn'
+    });
+    # Delete the old syspref
+    $dbh->do(q{
+        DELETE IGNORE FROM systempreferences
+        WHERE variable='RefundLostItemFeeOnReturn'
+    });
+
+    print "Upgrade to $DBversion done (Bug 14048: Change RefundLostItemFeeOnReturn to be branch specific)\n";
     SetVersion($DBversion);
 }
 
