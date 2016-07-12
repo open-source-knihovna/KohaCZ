8835630882b3868efac4ee428ade3ef46b5f1f5d
Kyle M Hall <kyle@bywatersolutions.com>
2016-07-12 14:48:03 +0000

    Bug 14826 - Resurrect account offsets table
    The account offsets table should be used to track increments and
    decrements of fines via payments and credits, as well as fine accruals.
    It should be able to match fees to payments and visa versa, so we can
    know which fee was paid by a given payment, and which payments applied
    to a given fee.
    
    Test Plan:
    1) Apply this patch
    2) Run updatedatabase
    3) Note the table accountoffsets has been renamed to account_offsets
    4) Ensure fine generation creates offsets
    5) Ensure creating a manual invoice creates an offset
    6) Ensure a lost item charge creates an offset
    7) Ensure Reverse Payment creates an offset
    8) Ensure a payment creates an offset
    9) Ensure a payment for multiple fees creates an offset for each
    10) Ensure writeoffs create offsets

--- C4/Overdues.pm
+++ C4/Overdues.pm
@@ -38,6 +38,7 @@ use Koha::Account::Line;
 use Koha::Account::Lines;
 use Koha::IssuingRules;
 use Koha::Libraries;
+use Koha::Account::Offset;
 
 use vars qw(@ISA @EXPORT);
 
@@ -592,6 +593,14 @@ sub UpdateFine {
                     accounttype   => 'FU',
                 }
             )->store();
+
+            Koha::Account::Offset->new(
+                {
+                    debit_id => $accountline->id,
+                    type     => 'Fine Update',
+                    amount   => $diff,
+                }
+            )->store();
         }
     } else {
         if ( $amount ) { # Don't add new fines with an amount of 0
@@ -617,6 +626,14 @@ sub UpdateFine {
                     lastincrement     => $amount,
                     accountno         => $nextaccntno,
                     issue_id          => $issue_id,
+                }
+            )->store();
+
+            Koha::Account::Offset->new(
+                {
+                    debit_id => $accountline->id,
+                    type     => 'Fine',
+                    amount   => $amount,
                 }
             )->store();
         }
