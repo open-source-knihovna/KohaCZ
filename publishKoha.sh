3f0adc160171f0bdacf56a61ea1c800cad780e62
Josef Moravec <josef.moravec@gmail.com>
2016-09-23 14:46:20 +0200

    Wait for keypress before scp/ssh in publish script

--- publishKoha.sh
+++ publishKoha.sh
@@ -14,11 +14,14 @@ msgcat misc/translator/po/cs-CZ-pref-orig.po misc/translator/po/cs-CZ-pref-kohac
 
 DEB_BUILD_OPTIONS=nocheck ./debian/build-git-snapshot -r ~/debian -v $VERSION -d --noautoversion
 
+echo "Build finished, press any key to upload deb files to repository server..."
+read -n 1 -s
+
 scp "../debian/koha-deps_${VERSION}_all.deb" $SERVER:/root/kohadeb/
 scp "../debian/koha-perldeps_${VERSION}_all.deb" $SERVER:/root/kohadeb/
 scp "../debian/koha-common_${VERSION}_all.deb" $SERVER:/root/kohadeb/
 
-ssh root@192.168.1.113 <<ENDSSH
+ssh root@$SERVER <<ENDSSH
     cd /root/
     aptly repo add kohacz kohadeb/koha-deps_${VERSION}_all.deb
     aptly repo add kohacz kohadeb/koha-perldeps_${VERSION}_all.deb
