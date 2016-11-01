fe8c56b2b35a69b6da3f67765042d86b75fb86e4
Radek Šiman <rbit@rbit.cz>
2016-11-01 08:20:30 +0100

    Bug 17509: Added NotifyToReturnItemWhenHoldIsPlaced syspref
    This system preference will allow to ask patrons to return items
    whenever a new hold is placed on them
    
    Test plan:
    1) Open System preferences / Circulation / Holds Policy
    2) Change value from Enable to Don't enable and vice versa
    3) Check NotifyToReturnItemWhenHoldIsPlaced is changed in database
    
    Signed-off-by: Josef Moravec <josef.moravec@gmail.com>

---
+++ installer/data/mysql/atomicupdate/bug_17509_add_NotifyToReturnItemWhenHoldIsPlaced_syspref.sql
@@ -0,0 +1,1 @@
+INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES ('NotifyToReturnItemWhenHoldIsPlaced', '0', 'If ON, notifies the borrowers to return an item whenever a hold is placed on it', NULL, 'YesNo');
