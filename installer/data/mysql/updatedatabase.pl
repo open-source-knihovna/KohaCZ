17c9c9428bb918030fbce1af437e8475a98b0d4c
Josef Moravec <josef.moravec@gmail.com>
2017-04-26 10:23:50 +0000

    Move atomic database update files to update script

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -14095,6 +14095,93 @@ if ( CheckVersion($DBversion) ) {
     });
 
     print "Upgrade to $DBversion done (Bug 14146 - Additional days are not added to restriction period when checking-in several overdues for same patron)'\n";
+
+    $dbh->do(q{
+        INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES
+        ('NotifyToReturnItemWhenHoldIsPlaced', '0', 'If ON, notifies the patrons to return an item when ever a hold is placed on it', NULL, 'YesNo');
+    });
+
+    $dbh->do(q{
+        INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES
+        ('NotifyToReturnItemFromLibrary', 'ItemHomeLibrary', 'Restricts what library to take into consi deration when notifying patrons to return items on hold', 'RequestorLibrary|ItemHomeLibrary|AnyLibrary', 'Choice');
+    });
+    print "Bug 17509: Add NotifyToReturnItemWhenHoldIsPlaced system preferences\n";
+
+    $dbh->do(q{
+        INSERT IGNORE INTO `letter` (module, code, branchcode, name, is_html, title, content, message_transport_type)
+        VALUES ('reserves','HOLDPLACED_CONTACT','','Hold Placed on Item, Contact Patrons','0','Item Return Required','Dear <<borrowers.firstname>> <<borrowers.surname>>,\r\na hold has been placed on the following item: <<biblio.title>> (<<biblio.biblionumber>>) by another patron. Please, return it as soon as possible.\r\n\r\nThank you!','email');
+    });
+    print "Bug 17509: Add notification template to notify that a hold was placed on checked out item\n";
+
+    $dbh->do( "
+        CREATE TABLE IF NOT EXISTS account_debit_types (
+            type_code varchar(32) NOT NULL,
+            default_amount decimal(28,6) NULL,
+            description varchar(200) NULL,
+            can_be_deleted tinyint NOT NULL DEFAULT '1',
+            can_be_added_manually tinyint(4) NOT NULL DEFAULT '1',
+            PRIMARY KEY (type_code)
+        ) COMMENT='' ENGINE='InnoDB' COLLATE 'utf8_unicode_ci'
+    " );
+
+    $dbh->do( "
+        CREATE TABLE IF NOT EXISTS account_credit_types (
+            type_code varchar(32) NOT NULL,
+            description varchar(200) NULL,
+            can_be_deleted tinyint NOT NULL DEFAULT '1',
+            can_be_added_manually tinyint(4) NOT NULL DEFAULT '1',
+            PRIMARY KEY (type_code)
+        ) COMMENT='' ENGINE='InnoDB' COLLATE 'utf8_unicode_ci'
+    " );
+
+    $dbh->do( "
+        INSERT IGNORE INTO account_debit_types (type_code, default_amount, description, can_be_deleted, can_be_added_manually) VALUES
+            ('A',   NULL,   'Account management fee',   0,  1),
+            ('F',   NULL,   'Overdue fine', 0,  1),
+            ('FU',  NULL,   'Accruing overdue fine',    0,  0),
+            ('L',   NULL,   'Lost item',    0,  1),
+            ('LR',  NULL,   'Lost and returned',    0,  0),
+            ('M',   NULL,   'Sundry',   0,  1),
+            ('N',   NULL,   'New card', 0,  1),
+            ('O',   NULL,   'Overdue fine', 0,  0),
+            ('Rent',    NULL,   'Rental fee',   0,  1),
+            ('Rep', NULL,   'Replacement',  0,  0),
+            ('Res', NULL,   'Reserve charge',   0,  1)
+    " );
+
+    $dbh->do( "
+        INSERT IGNORE INTO account_debit_types (type_code, default_amount, description, can_be_deleted, can_be_added_manually)
+        SELECT SUBSTR(authorised_value,1,5), lib, authorised_value, 1, 1
+        FROM authorised_values WHERE category='MANUAL_INV'
+    " );
+
+    $dbh->do( "
+        INSERT IGNORE INTO account_credit_types (type_code, description, can_be_deleted, can_be_added_manually) VALUES
+            ('C',   'Credit',   0, 1),
+            ('CC',  'Credit card',  0, 0),
+            ('CR',  'Refunded found lost item', 0, 0),
+            ('FFOR',    'Forgiven overdue fine',    0, 0),
+            ('FOR', 'Forgiven', 0, 1),
+            ('OL',  'Other online payment service', 0, 0),
+            ('Pay', 'Cash', 0, 0),
+            ('Pay00',   'Cash via SIP2',    0, 0),
+            ('Pay01',   'VISA via SIP2',    0, 0),
+            ('Pay02',   'Credit card via SIP2', 0, 0),
+            ('PayPa',   'PayPal',   0, 0),
+            ('W',   'Write Off',    0, 0)
+    " );
+
+    $dbh->do( "
+        ALTER TABLE `accountlines` CHANGE COLUMN `accounttype` `accounttype` varchar(32) default NULL
+    " );
+
+    $dbh->do( "
+        ALTER IGNORE TABLE `accountlines`  ADD `paymenttype` varchar(32) COLLATE 'utf8_unicode_ci' NULL AFTER `accounttype`
+    " );
+    print "Account types update done\n");
+
+    print "Upgrade to $DBversion done (Koha 16.11.06)\n;
+
     SetVersion($DBversion);
 }
 
