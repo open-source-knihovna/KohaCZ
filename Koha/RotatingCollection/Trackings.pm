00f39b83d2f7c58438149e61da93a1fefd71d8e3
Josef Moravec <josef.moravec@gmail.com>
2017-05-17 11:06:27 +0000

    Bug 18606: Add RotatingCollectionTracking objects

---
+++ Koha/RotatingCollection/Trackings.pm
@@ -0,0 +1,58 @@
+package Koha::RotatingCollection::Trackings;
+
+# Copyright Josef Moravec 2017
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use Modern::Perl;
+
+use Carp;
+
+use Koha::Database;
+
+use Koha::RotatingCollection::Tracking;
+
+use base qw(Koha::Objects);
+
+=head1 NAME
+
+Koha::RotatingCollection::Trackings - Koha Rotating collection trackings Object set class
+
+=head1 API
+
+=head2 Class Methods
+
+=cut
+
+=head3 type
+
+=cut
+
+sub _type {
+    return 'CollectionsTracking';
+}
+
+sub object_class {
+    return 'Koha::RotatingCollection::Tracking';
+}
+
+=head1 AUTHOR
+
+Josef Moravec <josef.moravec@gmail.com>
+
+=cut
+
+1;
