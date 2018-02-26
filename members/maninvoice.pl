70779359e41f95e323f0c8b59782ff2aa24d7575
Josef Moravec <josef.moravec@gmail.com>
2018-02-26 10:02:34 +0000

    KohaCZ: Clean manual invoice script

--- members/maninvoice.pl
+++ members/maninvoice.pl
@@ -27,18 +27,12 @@ use Modern::Perl;
 use C4::Auth;
 use C4::Output;
 use CGI qw ( -utf8 );
-use C4::Members;
 use C4::Accounts;
-use C4::Items;
-use C4::Members::Attributes qw(GetBorrowerAttributes);
 
 use Koha::Patrons;
-use Koha::Patron::Images;
-use Koha::Patron::Categories;
-use Koha::Account::DebitTypes;
-use Koha::Account::CreditTypes;
+use Koha::Items;
 
-my $input=new CGI;
+my $input = new CGI;
 my $flagsrequired = { borrowers => 1, updatecharges => 'remaining_permissions' };
 
 my $borrowernumber=$input->param('borrowernumber');
@@ -49,87 +43,21 @@ unless ( $patron ) {
     exit;
 }
 
-my $add=$input->param('add');
+my $add = $input->param('add');
 if ($add){
     if ( checkauth( $input, 0, $flagsrequired, 'intranet' ) ) {
         #  print $input->header;
-        my $barcode=$input->param('barcode');
-        my $itemnum;
-        if ($barcode) {
-            $itemnum = GetItemnumberFromBarcode($barcode);
+        my $barcode = $input->param('barcode');
+        my $item = Koha::Items->find({ barcode => $barcode });
+        my $itemnumber;
+        if ($item) {
+            $itemnumber = $item->itemnumber;
         }
-        my $desc=$input->param('desc');
-        my $amount=$input->param('amount');
-        my $type=$input->param('type');
-        my $note    = $input->param('note');
-        my $error   = manualinvoice( $borrowernumber, $itemnum, $desc, $type, $amount, $note );
-        if ($error) {
-            my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
-                {   template_name   => "members/maninvoice.tt",
-                    query           => $input,
-                    type            => "intranet",
-                    authnotrequired => 0,
-                    flagsrequired   => $flagsrequired,
-                    debug           => 1,
-                }
-            );
-            if ( $error =~ /FOREIGN KEY/ && $error =~ /itemnumber/ ) {
-                $template->param( 'ITEMNUMBER' => 1 );
-            }
-            $template->param( 'ERROR' => $error );
-            output_html_with_http_headers $input, $cookie, $template->output;
-        } else {
-            print $input->redirect("/cgi-bin/koha/members/boraccount.pl?borrowernumber=$borrowernumber");
-            exit;
-        }
+        my $desc = $input->param('desc');
+        my $amount = $input->param('amount');
+        my $type = $input->param('type');
+        my $error = manualinvoice( $borrowernumber, $itemnumber, $desc, $type, $amount );
     }
-} else {
-
-    my ($template, $loggedinuser, $cookie) = get_template_and_user({
-        template_name   => "members/maninvoice.tt",
-        query           => $input,
-        type            => "intranet",
-        authnotrequired => 0,
-        flagsrequired   => { borrowers => 1,
-                             updatecharges => 'remaining_permissions' },
-        debug           => 1,
-    });
-					
-  # get authorised values with type of MANUAL_INV
-  my @invoice_types;
-  my $dbh = C4::Context->dbh;
-  my $sth = $dbh->prepare('SELECT * FROM authorised_values WHERE category = "MANUAL_INV"');
-  $sth->execute();
-  while ( my $row = $sth->fetchrow_hashref() ) {
-    push @invoice_types, $row;
-  }
-  $template->param( invoice_types_loop => \@invoice_types );
-
-    if ( $patron->category->category_type eq 'C') {
-        my $patron_categories = Koha::Patron::Categories->search_limited({ category_type => 'A' }, {order_by => ['categorycode']});
-        $template->param( 'CATCODE_MULTI' => 1) if $patron_categories->count > 1;
-        $template->param( 'catcode' => $patron_categories->next->categorycode )  if $patron_categories->count == 1;
-    }
-
-    $template->param( adultborrower => 1 ) if ( $patron->category->category_type =~ /^(A|I)$/ );
-    $template->param( picture => 1 ) if $patron->image;
-
-    if (C4::Context->preference('ExtendedPatronAttributes')) {
-        my $attributes = GetBorrowerAttributes($borrowernumber);
-        $template->param(
-            ExtendedPatronAttributes => 1,
-            extendedattributes => $attributes
-        );
-    }
-
-    $template->param(%{ $patron->unblessed });
-    $template->param(
-        finesview      => 1,
-        borrowernumber => $borrowernumber,
-        categoryname   => $patron->category->description,
-        is_child       => ($patron->category->category_type eq 'C'),
-    );
-    output_html_with_http_headers $input, $cookie, $template->output;
 }
 print $input->redirect("/cgi-bin/koha/members/boraccount.pl?borrowernumber=$borrowernumber");
 exit;
